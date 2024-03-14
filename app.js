c18e449b2731b5fbc46f21f19c079a26d30485e9
vgextezil <hakman.vasyl@chnu.edu.ua>
2024-03-14 16:26:54 +0200

    1 step

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import express from 'express';
 const PORT = 3000;
 const server = express();
 console.log('Nodemon1');
-
 server.listen(PORT, () =>{
     console.log(`Server has been started on port ${PORT}`);
 })
